c8ce34bc261f90dd8535e55008a718f38f621675
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-11-20 17:38:11 +0900

    Initial Commit

---
+++ EzSample_NSNotification/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EzSample_NSNotification
+//
+//  Created by Tomohiro Kumagai on H27/11/20.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
